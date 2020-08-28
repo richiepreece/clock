6bedba969f65e29f5676341aa4bf6992575841c0
Richie Preece <preece@adobe.com>
2020-08-28 16:14:18 -0600

    add build folder

---
+++ build/precache-manifest.03c574d3845e3e99f88bd4d2e902a954.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bafa79342e5cd47d92ea81f6a6bc9ab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b64e4cc61342d509364",
+    "url": "/static/css/main.2aeaf1ae.chunk.css"
+  },
+  {
+    "revision": "30ed8c72838ab2acbc72",
+    "url": "/static/js/2.3661b093.chunk.js"
+  },
+  {
+    "revision": "3ac95e0a166b0a8f534792abdb090c75",
+    "url": "/static/js/2.3661b093.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b64e4cc61342d509364",
+    "url": "/static/js/main.f2b4712d.chunk.js"
+  },
+  {
+    "revision": "9b12f03f412b5377ea8e",
+    "url": "/static/js/runtime-main.f37dd4a2.js"
+  }
+]);
