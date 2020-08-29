95d6252e71068af433d92601b3a0e59efadde151
Richie Preece <preece@adobe.com>
2020-08-28 21:11:30 -0600

    doc files

---
+++ docs/precache-manifest.24804c26b515e3a24421f7d6dc39877d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fddccd1db4fa4c94149f7e5b26ca64c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "efe90d31672af42ddbc2",
+    "url": "/static/css/main.1a2d200b.chunk.css"
+  },
+  {
+    "revision": "6662d366cdfad9d233a0",
+    "url": "/static/js/2.85c3f03f.chunk.js"
+  },
+  {
+    "revision": "3ac95e0a166b0a8f534792abdb090c75",
+    "url": "/static/js/2.85c3f03f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efe90d31672af42ddbc2",
+    "url": "/static/js/main.5fc8cb01.chunk.js"
+  },
+  {
+    "revision": "9b12f03f412b5377ea8e",
+    "url": "/static/js/runtime-main.f37dd4a2.js"
+  }
+]);
